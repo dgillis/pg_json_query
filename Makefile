1fcf13c711f19b74b99648fc4ca1ee2f6077fea4
dgillis <d@duncangillis.com>
2015-07-24 17:07:11 -0300

    Added contains/contained ops and upgraded version to v0.2

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 EXTENSION = json_query
 
-DATA = build/json_query--0.1.sql
+DATA = build/json_query--*.sql
 PG_CONFIG = pg_config
 PGXS := $(shell $(PG_CONFIG) --pgxs)
+
+CURR_VERSION = 0.2
+CURR_BUNDLE = build/json_query--${CURR_VERSION}.sql
 
 libsqldir = libsql
 
@@ -10,16 +13,13 @@ data_includes =	${libsqldir}/init_schema.sql \
 		${libsqldir}/utils.sql \
 		${libsqldir}/types.sql \
 		${libsqldir}/columns.sql \
-		${libsqldir}/ops.sql \
 		${libsqldir}/casts.sql \
+		${libsqldir}/ops.sql \
 		${libsqldir}/filter.sql \
 		${libsqldir}/column_expr.sql \
 		${libsqldir}/creator_funcs.sql
 
-$(DATA): ${data_includes}
-	cat $^ > ${DATA}
-
-clean:
-	rm -f ${DATA}
+bundle: ${data_includes}
+	cat $^ > ${CURR_BUNDLE}
 
 include $(PGXS)
--- libsql/ops.sql
+++ libsql/ops.sql
@@ -124,6 +124,55 @@ $$;
 
 
 
+-- contains (i.e., "@>" gin operator)
+
+-- Fallback implementation: attempt to cast the textual representation of the value to whatever
+-- the column's type is.
+create function _pg_json_query._contains(col anyelement, filt jsonb, _coltyp anyelement default null)
+returns boolean language sql immutable as $$
+  select col @> _pg_json_query._cast(filt->>'value', _coltyp)
+$$;
+
+-- jsonb implementation.
+create function _pg_json_query._contains(col jsonb, filt jsonb, _coltyp jsonb default null)
+returns boolean language sql immutable as $$ select col @> filt->'value' $$;
+
+-- json implementation
+create function _pg_json_query._contains(col json, filt jsonb, _coltyp json default null)
+returns boolean language sql immutable as $$ select col::jsonb @> filt->'value' $$;
+
+
+-- contained ("<@"). Functions analoguous to the contains functions.
+create function _pg_json_query._contained(col anyelement, filt jsonb, _coltyp anyelement default null)
+returns boolean language sql immutable as $$
+  select col <@ _pg_json_query._cast(filt->>'value', _coltyp)
+$$;
+
+-- jsonb implementation.
+create function _pg_json_query._contained(col jsonb, filt jsonb, _coltyp jsonb default null)
+returns boolean language sql immutable as $$ select col <@ filt->'value' $$;
+
+-- json implementation
+create function _pg_json_query._contained(col json, filt jsonb, _coltyp json default null)
+returns boolean language sql immutable as $$ select col::jsonb <@ filt->'value' $$;
+
+
+-- not contains
+create function _pg_json_query._notcontains(col anyelement, filt jsonb, _coltyp anyelement default null)
+returns boolean language sql immutable as $$
+  select not _pg_json_query._contains(col, filt, _coltyp);
+$$;
+
+
+-- not contained
+create function _pg_json_query._notcontained(col anyelement, filt jsonb, _coltyp anyelement default null)
+returns boolean language sql immutable as $$
+  select not _pg_json_query._contains(col, filt, _coltyp);
+$$;
+
+
+
+
 create function _pg_json_query._apply_op(op text, col anyelement, filt jsonb)
 returns boolean language sql immutable as $$
   select case op
@@ -143,6 +192,10 @@ returns boolean language sql immutable as $$
     when 'iendswith' then _pg_json_query._iendswith(col, filt)
     when 'exists' then _pg_json_query._exists(col, filt)
     when 'notexists' then _pg_json_query._notexists(col, filt)
+    when 'contains' then _pg_json_query._contains(col, filt)
+    when 'notcontains' then _pg_json_query._notcontains(col, filt)
+    when 'contained' then _pg_json_query._contained(col, filt)
+    when 'notcontained' then _pg_json_query._notcontained(col, filt)
     -- Aliases
     when 'gte' then _pg_json_query._ge(col, filt)
     when 'lte' then _pg_json_query._le(col, filt)
