71f41ec284c6ca400762f52df23073c5768adffd
Duncan Gillis <d@duncangillis.com>
2017-10-17 15:10:31 -0300

    Fixed incorrect inclusion of unknown types among default column types (needed for pg10 compat).

--- libsql/db_info.sql
+++ libsql/db_info.sql
@@ -67,16 +67,22 @@ $$;
 -- non-unknown types, which are defined and which are not internal. The
 -- rows consist of a textual representation of the type (suitable to
 -- substitute into dynamic SQL) and the types OID.
-create or replace view _pg_json_query._default_row_types as (
+create or replace view _pg_json_query._default_row_types_with_typeinfo as (
   select
     t.oid::regtype::text as type_name,
-    t.oid as type_oid
+    t.oid as type_oid,
+    t.*
   from pg_type t join pg_namespace n on t.typnamespace = n.oid
   where
     n.nspname = 'pg_catalog' and -- built-ins
-    typcategory not in ('P', 'x') and -- neither psuedo nor unknown
+    typcategory not in ('P', 'X') and -- neither psuedo nor unknown
     typname::text !~* '^pg_.*' and -- non-internal
     typisdefined -- is defined
+);
+-- Compact view consisting only of type name and oid.
+create or replace view _pg_json_query._default_row_types as (
+  select type_name, type_oid
+  from _pg_json_query._default_row_types_with_typeinfo
 );
 
 
